ec773aabbdc2442b842f2b98d86dcccbf01e0fee
mst.christian_ <acevesrocha2009@gmail.com>
2025-07-30 20:16:16 +0200

    Set up basic plugin structure

---
+++ src/main/java/github/mstchristian/goatcore/GoatCore.java
@@ -0,0 +1,18 @@
+//Copyright (c) 2025 mst.christian_
+//Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package github.mstchristian.goatcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoatCore extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("GoatCore has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("GoatCore has been disabled!");
+    }
+}
